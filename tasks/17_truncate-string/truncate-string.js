e2d5b7602c1a345c8fee6304304ebe881ea9cfab
JovanovicNatalija <natalija.jovanovic996@gmail.com>
2020-02-13 15:37:06 +0100

    add brackets for the body of if statements

--- tasks/17_truncate-string/truncate-string.js
+++ tasks/17_truncate-string/truncate-string.js
@@ -1,7 +1,11 @@
 export function truncateString(string, maxLength) {
-  if (string.length <= maxLength) return string;
+  if (string.length <= maxLength) {
+    return string;
+  }
 
-  if (maxLength <= 3) return string.slice(0, maxLength) + '...';
+  if (maxLength <= 3) {
+    return string.slice(0, maxLength) + '...';
+  }
 
   return string.slice(0, maxLength - 3) + '...';
 }
