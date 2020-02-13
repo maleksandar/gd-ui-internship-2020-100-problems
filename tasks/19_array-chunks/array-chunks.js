3bdcd861edd09b4b72e913a51fbdec99d75b6d10
JovanovicNatalija <natalija.jovanovic996@gmail.com>
2020-02-13 16:16:06 +0100

    extract array.length and use loop instead of .reduce

--- tasks/19_array-chunks/array-chunks.js
+++ tasks/19_array-chunks/array-chunks.js
@@ -1,10 +1,14 @@
 export function arrayChunks(array, size) {
-  return array.reduce((acc, item, index, array) => {
-    if (!(index % size)) {
-            array.length - index < size ?
-                acc.push(array.slice(index)) :
-                acc.push(array.slice(index, index + size));
+  const n = array.length;
+  const groups = [];
+
+  for (let i = 0; i < n; i += size) {
+    if (n - i < size) {
+      groups.push(array.slice(i));
+    } else {
+      groups.push(array.slice(i, i + size));
     }
-    return acc;
-  }, []);
+  }
+
+  return groups;
 }
