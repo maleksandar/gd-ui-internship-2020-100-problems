14acf02c9e018abf1a395d644d42f26e2704f2e8
JovanovicNatalija <natalija.jovanovic996@gmail.com>
2020-02-13 15:05:37 +0100

    extract to function capitalizeWord

--- tasks/12_title-case/title-case.js
+++ tasks/12_title-case/title-case.js
@@ -1,7 +1,10 @@
 export function titleCase(string) {
   return string
       .split(' ')
-      .map((word) => word.charAt(0).toUpperCase()
-      + word.substring(1).toLowerCase())
+      .map(capitalizeWord)
       .join(' ');
+
+  function capitalizeWord(word) {
+    return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+  }
 }
