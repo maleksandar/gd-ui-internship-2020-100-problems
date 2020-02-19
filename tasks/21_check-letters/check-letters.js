6acfe31d50842221be9db210e693e62853f1fb45
JovanovicNatalija <natalija.jovanovic996@gmail.com>
2020-02-19 12:52:44 +0100

    Extract to function convertToArray(string) and rename firstArray and secondArray to firstString and secondString because it's actually a string

--- tasks/21_check-letters/check-letters.js
+++ tasks/21_check-letters/check-letters.js
@@ -1,10 +1,12 @@
-export function checkLetters([firstArray, secondArray]) {
-  return secondArray
-      .toLowerCase()
-      .split('')
+export function checkLetters([firstString, secondString]) {
+  return convertToArray(secondString)
       .reduce((acc, character) => {
-        return acc && isCharacterContainedInArray(character, firstArray);
+        return acc && isCharacterContainedInArray(character, firstString);
       }, true);
+
+  function convertToArray(string) {
+    return string.toLowerCase().split('');
+  }
 
   function isCharacterContainedInArray(character, array) {
     return array.toLowerCase().indexOf(character) !== -1;
