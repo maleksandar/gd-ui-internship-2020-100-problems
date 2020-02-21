69ba19e0c07df89cefa0dd415975cdee8bf1c655
vstefanovic-grid <vstefanovc@griddynamics.com>
2020-02-21 09:07:16 +0100

    Make optimization changes to tasks 24 and 33

--- tasks/24_index-to-insert/index-to-insert.js
+++ tasks/24_index-to-insert/index-to-insert.js
@@ -1,10 +1,11 @@
 export function getIndexToInsert(arr, num) {
-  const cloneArr = [...arr].sort((a, b) => a-b);
-  if (num <= cloneArr[0]) return 0;
-  if (num > cloneArr[cloneArr.length - 1]) return arr.length;
-  for (let i = 0; i < cloneArr.length - 1; i++) {
+  const cloneArr = [...arr].sort((a, b) => a - b);
+  let i = 0;
+  while (num > cloneArr[i]) {
     if (num <= cloneArr[i + 1]) {
       return i + 1;
     }
+    i++;
   }
+  return i;
 }
--- tasks/33_array-plus-array/array-plus-array.js
+++ tasks/33_array-plus-array/array-plus-array.js
@@ -1,10 +1,10 @@
 export function arrayPlusArray(arr1, arr2) {
   const arr3 = [];
-  const maxLength = Math.max(arr1.length, arr2.length);
-  for (let i = 0; i < Math.min(arr1.length, arr2.length); i++) {
-    arr3.push(arr1[i] + arr2[i]);
+  const maxLength = arr1.length > arr2.length? arr1.length : arr2.length;
+
+  for (let i = 0; i < maxLength; i++) {
+    arr3.push((arr1[i] || 0) + (arr2[i] || 0));
   }
-  arr1 = arr1.slice(maxLength);
-  arr2 = arr2.slice(maxLength);
-  return [...arr3, ...arr1, ...arr2];
+
+  return arr3;
 }
