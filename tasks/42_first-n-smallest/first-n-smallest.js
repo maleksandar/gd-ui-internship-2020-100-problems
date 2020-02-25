c5d8d1c6865dfecefe6bc9dbcb67487867858d65
JovanovicNatalija <natalija.jovanovic996@gmail.com>
2020-02-25 15:01:54 +0100

    More efficient solution, using algorithm kth smallest element  - complexity O(n)

--- tasks/42_first-n-smallest/first-n-smallest.js
+++ tasks/42_first-n-smallest/first-n-smallest.js
@@ -1,33 +1,31 @@
 export function firstNSmallest(arrayOfNumbers, n) {
-  const len = arrayOfNumbers.length;
-  const arrayOfObjects = [];
+  if (n === 0 || arrayOfNumbers.length === 0) {
+    return [];
+  }
+  const valueOfNthSmallest = nthSmallest(arrayOfNumbers, n);
+  return arrayOfNumbers.filter((x) => x <= valueOfNthSmallest).slice(0, n);
+}
 
-  for (let i = 0; i < len; i++) {
-    let numberOfSmaller = 0;
-
-    for (let j = 0; j < len; j++) {
-      if (arrayOfNumbers[j] < arrayOfNumbers[i]) {
-        numberOfSmaller++;
-      }
+function nthSmallest(array, n) {
+  if (n === 0 || array.length === 0) {
+    throw new Error(`Can't find 0th smallest element`);
+  }
+  const pivotIndex = 0;
+  const pivot = array[pivotIndex];
+  const smallerThanPivot = [];
+  const largerOrEqualThanPivot = [];
+  array.slice(pivotIndex + 1).forEach((element) => {
+    if (element < pivot) {
+      smallerThanPivot.push(element);
+    } else {
+      largerOrEqualThanPivot.push(element);
     }
-
-    arrayOfObjects.push({
-      number: arrayOfNumbers[i],
-      numberOfSmaller: numberOfSmaller,
-      index: i,
-    });
-  }
-
-  const sortedByNumberOfSmaller = arrayOfObjects.sort(compareNumberOfSmaller);
-  const arrayOfNSmallest = sortedByNumberOfSmaller.slice(0, n);
-  const sortedByIndexes = arrayOfNSmallest.sort(compareIndexes);
-
-  return sortedByIndexes.map((object) => object.number);
-
-  function compareNumberOfSmaller(a, b) {
-    return a.numberOfSmaller - b.numberOfSmaller;
-  }
-  function compareIndexes(a, b) {
-    return a.index - b.index;
+  });
+  if (smallerThanPivot.length === n - 1) {
+    return pivot;
+  } else if (smallerThanPivot.length < n) {
+    return nthSmallest(largerOrEqualThanPivot, n - smallerThanPivot.length - 1);
+  } else {
+    return nthSmallest(smallerThanPivot, n);
   }
 }
