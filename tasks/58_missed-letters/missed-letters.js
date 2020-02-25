a4538cc24bc05fecd9004100b3e1847d12f9cd2d
Ivan Simic <ivansimic@Ivans-MacBook-Pro.local>
2020-02-25 13:54:32 +0100

    Solution to problem 48

---
+++ tasks/58_missed-letters/missed-letters.js
@@ -0,0 +1,11 @@
+
+
+export function findLetter(str) {
+  for (let i=0; i<str.length-1; i++) {
+    if (str.charCodeAt(i+1) - str.charCodeAt(i) > 1) {
+      return String.fromCharCode((str.charCodeAt(i) + 1));
+    }
+  }
+
+  return undefined;
+}
