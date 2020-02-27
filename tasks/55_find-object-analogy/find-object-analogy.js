3ccfb35832b43e5c5d9185e591fd8ece6789ad8f
JovanovicNatalija <natalija.jovanovic996@gmail.com>
2020-02-27 12:53:57 +0100

    Extraxt to isObjectSuperSetOf(object).

--- tasks/55_find-object-analogy/find-object-analogy.js
+++ tasks/55_find-object-analogy/find-object-analogy.js
@@ -1,8 +1,12 @@
 export function whatIsInAName(arrayOfObjects, object) {
+  return arrayOfObjects.filter(isSuperSetOf(object));
+}
+function isSuperSetOf(object) {
   const keys = Object.keys(object);
-  return arrayOfObjects.filter((obj) => {
+  return (obj) => {
     return keys.every((key) => {
       return obj[key] === object[key];
     });
-  });
+  };
 }
+
