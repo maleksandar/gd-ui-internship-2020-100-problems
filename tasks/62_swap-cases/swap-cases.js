838e7edeaef6a3b425ae53faa53d055859ccdddd
vstefanovic-grid <vstefanovic@griddynamics.com>
2020-03-03 12:26:10 +0100

    Change task 62 to use replace()

--- tasks/62_swap-cases/swap-cases.js
+++ tasks/62_swap-cases/swap-cases.js
@@ -1,6 +1,13 @@
 export function swapCases(str) {
-  return str.split('').map((char) => {
-    return char === char.toUpperCase()?
-       char.toLowerCase() : char.toUpperCase();
-  }).join('');
+  return str.replace(/[a-zA-Z]/g, (match) => {
+    return match === match.toUpperCase()?
+     match.toLowerCase() : match.toUpperCase();
+  });
 }
+
+// export function swapCases(str) {
+//   return str.split('').map((char) => {
+//     return char === char.toUpperCase()?
+//        char.toLowerCase() : char.toUpperCase();
+//   }).join('');
+// }
