b546950a485c72cb3668b6d353abb3cbf7393a81
bojana <bjovanovic@griddynamics.com>
2020-03-15 15:21:04 +0100

    Change tasks 63 and 69 according to PR review

--- tasks/63_insert-string/insert-string.js
+++ tasks/63_insert-string/insert-string.js
@@ -1,20 +1,18 @@
 /* eslint-disable no-unreachable */
 /* eslint-disable prefer-rest-params */
-export function insert(string) {
-  let newStr = '';
-
+export function insert(string, replaceString, position) {
   switch (arguments.length) {
     case 1:
       return string;
       break;
     case 2:
-      newStr= arguments[1] + string;
+      return replaceString + string;
       break;
     case 3:
-      newStr= string.slice(0, arguments[2])
-      + arguments[1] + string.slice(arguments[2]);
+      return string.slice(0, position)
+      + replaceString + string.slice(position);
       break;
+    default:
+      return '';
   }
-
-  return newStr;
 }
--- tasks/69_unpack-elements/unpack-elements.js
+++ tasks/69_unpack-elements/unpack-elements.js
@@ -1,9 +1,7 @@
 export function unpackArray(array) {
-  return array.reduce(function iter(acc, curr) {
-    if (Array.isArray(curr)) {
-      return curr.reduce(iter, acc);
-    }
-
-    return acc.concat(curr);
+  return array.reduce((acc, curr) => {
+    return acc.concat(
+      Array.isArray(curr) ? unpackArray(curr) : curr
+    );
   }, []);
 }
